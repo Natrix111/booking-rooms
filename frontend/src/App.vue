95d58d82516cfce2885fb0df5f63951020889990
karix <eivanov200606@mail.ru>
2024-11-27 23:14:41 +0700

    footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import MyHeader from "@/components/globals/MyHeader.vue";
+import MyFooter from "@/components/globals/MyFooter.vue";
 </script>
 
 <template>
   <MyHeader/>
+  <MyFooter/>
 </template>
 
 <style scoped>
