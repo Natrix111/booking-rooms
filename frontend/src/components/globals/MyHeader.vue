4a85562790988b6dc3e8a9f1d9d3e139b46d9f9d
karix <eivanov200606@mail.ru>
2024-11-28 15:19:51 +0700

    add mainInfo in header from api

--- frontend/src/components/globals/MyHeader.vue
+++ frontend/src/components/globals/MyHeader.vue
@@ -2,8 +2,8 @@
   <header class="bg-blue-500 text-white py-4 px-10">
     <div class="mx-auto flex justify-between items-center">
       <div>
-        <h1 class="text-xl font-bold">{{ headerData.siteName }}</h1>
-        <p>{{ headerData.slogan }}</p>
+        <h1 class="text-xl font-bold">{{ mainIfo.title }}</h1>
+        <p>{{ mainIfo.slogan }}</p>
       </div>
       <div class="flex space-x-20">
         <nav class="flex space-x-4">
@@ -13,7 +13,7 @@
         </nav>
         <div class="flex space-x-4">
           <a href="/">Вход / Регистрация</a>
-          <p class="underline">{{ headerData.city }}</p>
+          <p class="underline">{{ mainIfo.city }}</p>
         </div>
       </div>
     </div>
@@ -21,11 +21,27 @@
 </template>
 
 <script setup>
-const headerData = {
-  siteName: "Котейка",
-  city: "Томск",
-  slogan: "Лучший отель для вашего питомца"
+import axios from "axios";
+import {onMounted, ref} from "vue";
+
+const mainIfo = ref({
+  title: 'Котейка',
+  slogan: 'Слоган',
+  city: 'Москва',
+})
+
+const getMainInfo = async () => {
+  try {
+    const { data } = await axios.get('http://localhost:8080/api/info')
+    return data[0]
+  }catch(error) {
+    console.error(error)
+  }
 }
+
+onMounted(async () => {
+  mainIfo.value = await getMainInfo()
+})
 </script>
 
 <style scoped>
