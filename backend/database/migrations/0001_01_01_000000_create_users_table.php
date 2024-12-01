f00197e01e23f3cc0bf28d6150f6cc132e77a69e
scarione <scarioneq@gmail.com>
2024-12-01 07:14:01 +0700

    добавлена логика регистрации, логина и логаута

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,10 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar')->nullable();
+            $table->string('role')->default('client'); // доступ к функционалу админа будем ограничивать
+            $table->rememberToken();                                // групповыми политиками в зависимости от роли users
             $table->timestamps();
         });
 
