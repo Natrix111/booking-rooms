21e6488906d06d95dd78cb0237d2008ee126bdb8
karix <eivanov200606@mail.ru>
2024-12-10 18:08:46 +0700

    add amenities icons

--- frontend/src/components/catalogRooms/CatalogRoomsListItem.vue
+++ frontend/src/components/catalogRooms/CatalogRoomsListItem.vue
@@ -2,17 +2,22 @@
   <article
       @click="$router.push(`/rooms/${room.id}`)"
       class="item">
-    <img
-        :src="room.photos[0] ? `${storageUrl}${props.room.photos[0]}` : defaultPreview"
-         alt="Фото номера"
-         class="item-image" />
-    <h3>{{ room.name }}</h3>
-    <p>Цена: {{ room.price }} руб/сутки</p>
-    <p>Площадь: {{ room.area }} м²</p>
-    <div>Оснащение:
-      <span v-for="(amenity, index) in room.amenities" :key="index">
-        {{ amenity.name }}<span v-if="index < room.amenities.length - 1">, </span>
-      </span>
+      <img
+          :src="room.photos[0] ? `${storageUrl}${props.room.photos[0]}` : defaultPreview"
+          alt="Фото номера"
+          class="item-image" />
+    <div class="item-wrapper">
+      <h3>{{ room.name }}</h3>
+      <p>Цена: {{ room.price }} руб/сутки</p>
+      <p>Площадь: {{ room.area }} м²</p>
+      <div class="icons-wrapper">
+        <img
+            v-for="amenity in room.amenities"
+            :key="amenity.name"
+            :title="amenity.name"
+            :src="`${storageUrl}${amenity.img}`"
+            class="icon">
+      </div>
     </div>
   </article>
 </template>
@@ -31,10 +36,22 @@ const props = defineProps({
 
 <style scoped>
 .item {
-  @apply bg-white p-4 rounded hover:shadow-lg duration-300 cursor-pointer overflow-hidden;
+  @apply bg-white rounded hover:shadow-lg duration-300 cursor-pointer overflow-hidden;
+}
+
+.item-wrapper {
+ padding: 0.5rem 1rem 1rem;
 }
 
 .item-image {
   @apply w-full h-40 object-cover;
+}
+
+.icons-wrapper {
+  @apply flex items-center space-x-3 mt-1.5;
+}
+
+.icon {
+  @apply h-6 w-6;
 }
 </style>
